9cc9298134de831a9f02d26e964e0485d9ce1ceb
kayvera <mikaylarvr@gmail.com>
2020-06-05 10:57:47 -0700

    server updates

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
+
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('API Running'));
+// Connect Database
+connectDB();
+
+// Init Middleware
+app.use(express.json());
+
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 const PORT = process.env.PORT || 5000;
 
